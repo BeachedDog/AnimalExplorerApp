d042e169385f58637569fdbdee9bbf815539daae
Kiefer Hillerich <beacheddogcreations@gmail.com>
2025-02-07 14:10:52 -0500

    I have completed the inital design of the AnimalBase class and method. Some methods have been refactored.

--- AnimalExplorerApp/Program.cs
+++ AnimalExplorerApp/Program.cs
@@ -6,5 +6,7 @@ internal class Program
     private static void Main(string[] args)
     {
       //currently working on abstract class methods. see AnimalBase line 101.
+
+        //I would like to retain the original inputs for each animal so that they can be restored in case they are inaccurately changed. a reset button. 
     }
 }
--- AnimalPages/AnimalBase.cs
+++ AnimalPages/AnimalBase.cs
@@ -15,24 +15,24 @@ namespace AnimalPages.Data
             {
                 if (string.IsNullOrWhiteSpace(value))
                     throw new ArgumentException("The name cannot be empty");
-
-                _name = value;
+                else
+                    _name = value;
             }
         }
         public virtual string _class { get; protected set; }
         private string _description { get; set; }
-        private List<string> _food { get; set; }
-        private string _colors { get; set; }
+        private List<string> _foods { get; set; }
+        private List<string> _colors { get; set; }
 
-        private int _maxSize;
-        public int MaxSize
+        private int _weight;
+        public int Weight
         {
-            get { return _maxSize; }
+            get { return _weight; }
             set
             {
-                if (MaxSize <= 0)
+                if (Weight >= 0)
                 {
-                    _maxSize = MaxSize;
+                    _weight = Weight;
                 }
                 else
                 {
@@ -43,38 +43,49 @@ namespace AnimalPages.Data
         private string _nativeLocation { get; set; }
         private List<string> _facts { get; set; }
         private List<byte[]> _photos { get; set; }
-        private List<string> _personelComments { get; set; }
+        private List<string> _comments { get; set; }
 
 
-        public T CreateAnimal<T>(string name, string description, List<string> food, string colors, int maxSize,
-            string nativeLocation, List<string> facts, List<byte[]> photos, List<string> personelComments) where T : AnimalBase, new()
+        public T SetAnimalValues<T>( List<string> facts, List<byte[]> photos, List<string> personelComments) where T : AnimalBase, new()
         {
             // create methods for each of these questions
+            T animal = new T();
 
-            string animalName = NameSetter(name);
-            Name = animalName;
-            string animalDescription = DescriptionSetter(description);
-            _description = animalDescription;
-            Console.WriteLine("What does this animal eat for food?");
-            Console.WriteLine("What are the common colors of this animal?");
-            Console.WriteLine("About how large are these animals? (please enter a whole number in pounds)");
-            Console.WriteLine("What areas is this animal native to?");
-            Console.WriteLine("Do you have any facts about this animal you would like to add? (press 1 for yes or 2 for no)");
-            Console.WriteLine("Do you have any photos of this animal you would like to add?");
-            Console.WriteLine("Do you have any comments or thoughts you would like to add about this animal? (press 1 for yes or 2 for no)");
+            //the way this is structured, this method must be called as the following example using the subclass fish: 'var fish = SetAnimalValues<Fish>'.
+            //This will create an instance of the subclass with the correct typing. 
+
+            string animalName = NameSetter();
+            animal.Name = animalName;
+            string animalDescription = DescriptionSetter();
+            animal._description = animalDescription;
+            List<string> initialAnimalFoods = FoodAdder(_foods);
+            animal._foods = initialAnimalFoods;
+            List<string> animalColors = ColorSetter();
+            animal._colors = animalColors;
+            int animalWeight = WeightSetter();
+            animal._weight = animalWeight;
+            string animalNativeLocation = NativeLocationSetter();
+            animal._nativeLocation = animalNativeLocation;
+            List<string> animalFacts = FactsAdder(_facts);
+            animal._facts = animalFacts;
+            Console.WriteLine("**no current method to add photos**"); // add this method when not using a console for UI
+            List<string> animalComments = CommentAdder(_comments);
+            animal._comments = animalComments;
+
+            return animal;
         }
 
-        public string NameSetter(string name)
+        public string NameSetter()
         {
             Console.WriteLine("What is the name of the animal you would like to add?");
             while (true)
             {
-                Console.WriteLine("Animal names must have at least two letters");
+                Console.WriteLine("Animal names must have at least two characters");
                 string userInput = Console.ReadLine();
-                WordStandardizer(userInput, "There are not enough letters in that animal name :(");
+                WordStandardizer(userInput, "There are not enough characters in that animal name :(");
                 if (userInput != null)
                 {
-                    name = userInput;
+                    var name = userInput;
                     return name;
                 }
                 else
@@ -84,26 +95,104 @@ namespace AnimalPages.Data
             }
 
         }
-        public string DescriptionSetter(string description)
+
+        public string DescriptionSetter()
         {
             Console.WriteLine("What is a good description of the animal?");
             string userInput = Console.ReadLine();
-            description = userInput;
+            var description = userInput;
             return description;
         }
-        public string FoodSetter(List<string> finalFoodList)
-        {
-            Console.WriteLine("If you know what this animal eats for food, Press 1. Otherwise press any other key.");
-            int userInput = 0;
-            List<string> foods = new List<string>();
-            
-            
-            //the next thing to do is to use the word standarizer method and finish writting this food method. 
-            //then see line 52 comment for next task. 
-                    
 
-               
+        public List<string> FoodAdder(List<string> foods)
+        {
+            if (foods == null)
+            {
+                foods = new List<string>();
+            }
+            Console.WriteLine("Enter 1 if you have foods to add. Enter any other key if not.");
+            int userInput = 0;
+            
+            try
+            {
+                userInput = int.Parse(Console.ReadLine());
+            }
+            catch (Exception) 
+            {
+                return foods;
+            }
+            if (userInput == 1) 
+            {
+                Console.WriteLine("Please enter the foods you wish to add");
+                string stringToBeListed = Console.ReadLine();
+                List<string> newList = ListCreator(stringToBeListed);
+                foreach (var item in newList)
+                    foods.Add(item);
+                return foods;
+            }
+            else
+            {
+                return foods;
+            }
         }
+
+        public List<string> ColorSetter()
+        {
+            Console.WriteLine("What colors are the animal made of?");
+            string colorsString = Console.ReadLine();
+            List<string> colors = ListCreator(colorsString);
+            return colors;
+        }
+
+        public int WeightSetter()
+        {
+            Console.WriteLine("Please enter a normal weight for this animal in whole pounds.");
+            try
+            {
+                int weight = int.Parse(Console.ReadLine());
+                return weight;
+            }
+            catch (Exception)
+            {
+                int weight = 0;
+                return weight;
+            }
+        }
+
+
+        public string NativeLocationSetter()
+        {
+            Console.WriteLine("What areas is this animal native to?");
+            string nativeLocation = Console.ReadLine();
+            nativeLocation = WordStandardizer(nativeLocation, "Please enter a location with more characters");
+            return nativeLocation;
+        }
+
+
+        public List<string> FactsAdder(List<string>_facts)
+        {
+            List<string> facts= InputCheckerSentences(_facts, "Do you have any interesting facts you would like to add for this animal? " +
+                "Enter 1 if you so, Enter any other key if not for now.", "Please enter a fact you would like to add.");
+            return facts;
+
+        }
+
+        public void PhotosAddrer()
+        {
+            //add this method when not using console for UI.
+        }
+
+
+        public List<string> CommentAdder(List<string> comments)
+        {
+            List<string> updatedComments = InputCheckerSentences(comments, "Enter 1 if there are comments or thoughts you would like to add.enter any other key if not.",
+                "Please enter your comment or thought.");
+            return updatedComments;
+        }
+
+
+
+        //Below is where the logic for resued items for the method setters/adders is held.
         public string WordStandardizer(string word, string errorMessage)
         {
             try
@@ -115,6 +204,54 @@ namespace AnimalPages.Data
             {
                 Console.WriteLine($"{errorMessage}");
                 return null;
+            }
+        }
+
+
+        public List<string> ListCreator(string words)
+        {
+            List<string> newList = words.Split(new[] { ' ', ',', }, StringSplitOptions.RemoveEmptyEntries)
+                .Where(word => !word.Contains("and"))
+                .ToList();
+            for (var i = 0; i < newList.Count; i++)
+            {
+                newList[i] = WordStandardizer(newList[i], "Please enter a word that has at least 2 characters");
+            }
+            return newList;
+        }
+
+
+        public List<string> InputCheckerSentences(List<string> items, string initialPrompt, string secondPrompt)
+        {
+            if (items == null)
+            {
+                items = new List<string>();
+            }
+            Console.WriteLine(initialPrompt);
+            int userInput = 0;
+
+            while(true)
+            {
+                try
+                {
+                    userInput = int.Parse(Console.ReadLine());
+                }
+                catch (Exception) 
+                {
+                    return items;
+                }
+                if (userInput == 1) 
+                {
+                    Console.WriteLine(secondPrompt);
+                    string item = Console.ReadLine();
+                    items.Add(item);
+                    Console.WriteLine("This has been added! Anything else you would like to add? Press 1 if so, press any other key if not.");
+                    userInput = 0;
+                }
+                else
+                {
+                    return items;
+                }   
             }
         }
     }
